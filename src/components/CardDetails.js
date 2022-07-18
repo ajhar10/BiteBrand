f29a1bd0a115e57a6f269d9796213303b32f62fb
ajhar10 <ajharrimon90@gmail.com>
2022-07-19 01:20:36 +0600

    quantity amount fixed

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Table } from 'react-bootstrap'
 import { useParams, useNavigate } from 'react-router-dom'
-import { useSelector,useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { DLT } from '../redux/actions/action'
 
 
@@ -9,13 +9,13 @@ const CardDetails = () => {
   const [data, setData] = useState([])
 
   const { id } = useParams();
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const dispatch = useDispatch();
 
   const getdata = useSelector(state => state.cartreducer.carts);
 
-// console.log(getdata);
+  // console.log(getdata);
   const compare = () => {
     let comparedata = getdata.filter((e) => {
       return e.id == id
@@ -23,12 +23,12 @@ const navigate = useNavigate();
     setData(comparedata)
   }
 
-  const removeItem = id=>{
+  const removeItem = id => {
     dispatch(DLT(id))
     navigate("/")
   }
 
-//It is used for selecting an item for details page on cart
+  //It is used for selecting an item for details page on cart
   useEffect(() => {
     compare();
   }, [id])
@@ -55,11 +55,16 @@ const navigate = useNavigate();
                             <p><strong>Price :</strong> ₹{item.price}</p>
                             <p><strong>Dishes :</strong>  {item.address}</p>
                             <p><strong>Total :</strong>  ₹ 70</p>
+                            {/* <div className='mt-5 d-flex justify-content-between align-items-center' style={{ width: 100, cursor: "pointer", background: "#ddd", color: "#111" }}>
+                              <span style={{ fontSize: 24 }}><i class="fa-solid fa-circle-minus"></i></span>
+                              <span style={{ fontSize: 24 }}>{item.qnty}</span>
+                              <span style={{ fontSize: 24 }}><i class="fa-solid fa-circle-plus"></i></span>
+                            </div> */}
                           </td>
                           <td>
                             <p><strong>Rating :</strong> <span style={{ background: "green", color: "#fff", padding: "2px", borderRadius: "4px" }}> {item.rating} ★</span></p>
                             <p><strong>Order Review :</strong> <span>{item.somedata}</span></p>
-                            <p><strong>Remove :</strong> <i className='fas fa-trash' style={{ color: "red", fontSize: "1.2rem" }} onClick={()=>removeItem(item.id)}></i> </p>
+                            <p><strong>Remove :</strong> <i className='fas fa-trash' style={{ color: "red", fontSize: "1.2rem" }} onClick={() => removeItem(item.id)}></i> </p>
                           </td>
                         </tr>
                       </Table>
--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -6,10 +6,22 @@ const INIT_STATE = {
 export const cartreducer = (state = INIT_STATE, action) => {
     switch (action.type) {
         case "ADD_CART":
-            return {
-                ...state,
-                carts: [...state.carts, action.payload]
+
+            const itemIndex = state.carts.findIndex(item => item.id == action.payload.id);
+            
+
+            if (itemIndex >= 0) {
+                state.carts[itemIndex].qnty += 1
+            } 
+            else {
+                const temp = { ...action.payload, qnty: 1 }
+                console.log(temp);
+                return {
+                    ...state,
+                    carts: [...state.carts, temp]
+                }
             }
+
         case "DLT_CART":
             const remainItem = state.carts.filter(item => item.id !== action.payload)
             return {
